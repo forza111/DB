af85a131a9a1fb539e584de8c39475b967523a2c
forza111 <nikitos.ionkin@yandex.ru>
2021-03-27 14:15:59 +0300

    Добавление модели TypeNumber и Telephone и отображение необходимой информации в admin панели

--- DB/accounts/admin.py
+++ DB/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Telephone, TypeNumber
+
+
+@admin.register(TypeNumber)
+class TypeNumberAdmin(admin.ModelAdmin):
+    '''Тип номера'''
+    list_display = ("type_number",)
+
+@admin.register(Telephone)
+class TelephoneAdmin(admin.ModelAdmin):
+    '''Телефон'''
+    list_display = ("telephone_number","user_id", "get_type_number")
+
+
 
 # Register your models here.
--- DB/accounts/models.py
+++ DB/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TypeNumber(models.Model):
+    type_number = models.CharField("Тип телефона",max_length=20)
+
+    def __str__(self):
+        return self.type_number
+
+
+class Telephone(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    telephone_number = models.CharField("Номер телефона",max_length=12)
+    type_number = models.ManyToManyField(TypeNumber, verbose_name="Тип телефона",
+                                         related_name="type_num")
+
+    def get_type_number(self):
+        return ",".join([str(tn) for tn in self.type_number.all()])
