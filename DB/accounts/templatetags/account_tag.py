5c94be324cdf701e71c175b00a153c87bc8330c1
forza111 <nikitos.ionkin@yandex.ru>
2021-04-06 15:52:12 +0300

    Добавление title к курсу EUR и DOL

--- DB/accounts/templatetags/account_tag.py
+++ DB/accounts/templatetags/account_tag.py
@@ -10,6 +10,18 @@ def get_rate_dollar():
     return Rate.objects.get(currency__name_currency='Доллар').rate
 
 @register.simple_tag()
+def get_date_rate_dollar():
+    '''Время послежнего обновления курса DOL по ЦБ'''
+    return Rate.objects.get(currency__name_currency='Доллар').date.strftime('%d/%m %H:%M')
+
+
+
+@register.simple_tag()
 def get_rate_euro():
     '''Курс Евро'''
     return Rate.objects.get(currency__name_currency='Евро').rate
+
+@register.simple_tag()
+def get_date_rate_euro():
+    '''Время послежнего обновления курса EUR по ЦБ'''
+    return Rate.objects.get(currency__name_currency='Евро').date.strftime('%d/%m %H:%M')
